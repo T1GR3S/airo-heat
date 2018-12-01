3a98baa2260b6516ed1fc18d93b0d510faa5ceb1
cybercamp <rjruizfdez@gmail.com>
2018-12-01 02:54:54 -0800

    New weights in Google Maps - Better looking

--- backend-dev/node_server/config.js
+++ backend-dev/node_server/config.js
@@ -3,6 +3,6 @@ var config = {};
 config.web = {};
 
 config.web.port = process.env.WEB_PORT || 3000;
-config.web.db = process.env.WEB_DATABASE || "database2.db";
+config.web.db = process.env.WEB_DATABASE || "database3.db";
 
 module.exports = config;
