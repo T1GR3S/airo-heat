c5c8d74a2cdad596d701f4e11cfacf82d3016f82
cybercamp <rjruizfdez@gmail.com>
2018-12-01 02:58:03 -0800

    Added install file

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo apt-get install node nodejs-legacy npm
+npm i express --build-from-source
+npm i sqlite3 --build-from-source
+npm i body-parser --build-from-source
+npm i ejs --build-from-source
