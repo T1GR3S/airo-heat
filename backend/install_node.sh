05fd54df6f602e9f7cdadda7b79ba3fe6524b90a
ricardojoserf <rjruizfdez@gmail.com>
2019-01-15 12:42:24 +0100

    Updated installation scripts

---
+++ backend/install_node.sh
@@ -0,0 +1,8 @@
+echo "Node server installation..."
+echo
+echo
+sudo apt-get install node nodejs-legacy npm
+sudo npm i express --build-from-source
+sudo npm i sqlite3 --build-from-source
+sudo npm i body-parser --build-from-source
+sudo npm i ejs --build-from-source
--- install.sh
+++ install.sh
@@ -1,12 +1,3 @@
-echo "Node server installation..."
-echo
-echo
-sudo apt-get install node nodejs-legacy npm
-npm i express --build-from-source
-npm i sqlite3 --build-from-source
-npm i body-parser --build-from-source
-npm i ejs --build-from-source
-
 echo
 echo
 echo "Aircrack-ng installation..."
